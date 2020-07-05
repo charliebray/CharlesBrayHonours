634390cd6006a016809c0f61c8a678b751c4f159
charliebray <charles.jbray@gmail.com>
2020-07-05 10:33:38 +1000

    Update ridgelines.py

--- ridgelines.py
+++ ridgelines.py
@@ -1,11 +1,15 @@
 from classes import *
 from scipy.optimize import fmin
 import scipy.integrate as integrate
-
 import math
 
+# This python script performs the ridge line fit, I've commented out the section that performs the fit for every single absorption line of sight I used
+#	and I have just left the code that performs the fit for one spectrum, and shows the plot.
+
+# Our emission .fits file
 filename_gass = "gass_smallest.fits"
 
+# All absorption .fits files that show absorption, along with their pixel positions
 LMC_list = [["0452-6823.1419.fits", 150, 150], ["0502-6625.1419.fits", 150, 150],\
                 ["0507-6657.1419.fits", 128, 148], ["0520-7312.1419.fits", 150, 149],\
                 ["0507-7144.1419.fits", 148, 149], ["0552-6814.1419.fits", 148, 152],\
@@ -17,39 +21,45 @@ LMC_list = [["0452-6823.1419.fits", 150, 150], ["0502-6625.1419.fits", 150, 150]
                 ["0522-7038.1419.fits", 150, 149], ["0540-6906.1419.fits", 151, 149],\
                 ["0547-7206.1419.fits", 150, 151]][11:12]
 
+# Lists that will store values later
 a_list = list()
-
 fwhm_list = list()
 
-# Significance and min FWHM values
+# Fitting criteria for absorption spectra
 abs_sig = 3.0
 abs_min = 3.0
 
+# Fitting criteria for emission spectra
 em_sig = 5.0
 em_min = 3.0
 
 for source in LMC_list:
 
+	# First fitting criteria for absorption spectra
 	abs_sig = 3.0
 	abs_min = 3.0
 
 	filename, x_pixel, y_pixel = source
 
+	# Create absorption spectrum
 	abs_obj = Abs_Spectra(filename, x_pixel, y_pixel)
 	abs_obj.raw_spectra()
 	abs_obj.T_bg(-50,-20)
 	abs_obj.optical_depth()
 
+	# Perform Gaussian decomposition on absorption spectrum
 	tau_spec = Plotting_Spectra(filename, abs_obj.vrad, abs_obj.tau_raw, abs_min, abs_sig)
 	tau_spec.gpp_parameters()
 
+	# Create emission spectrum
 	em_obj = Em_Spectra(filename_gass, abs_obj.ra, abs_obj.dec)
 	em_obj.raw_spectra()
 
+	# Perform Gaussian decomposition on emission spectrum
 	em_spec = Plotting_Spectra(filename_gass, em_obj.vrad, em_obj.T_B, em_min, em_sig)
 	em_spec.gpp_parameters()
 
-	# The following are their respective functions.
+	# The following are useful functions.
 
 	def gauss_func(amp, fwhm, mu, x):
 		return amp*np.exp(-4*np.log(2) * (x-mu)**2/(fwhm**2))
@@ -66,9 +76,7 @@ for source in LMC_list:
 	def N_func(x):
 		return ((1.823*(10**18))*T_B_model(x))
 
-	def iso_func(x):
-		return ((1.823*(10**18)) * T_B_model(x) * (1 + (tau_model(x)/2) + (tau_model(x)**2)/12) - ((tau_model(x)**4)/720))
-
+	# This stores our current components using our first fitting criteria, and we move onto the next fitting criteria
 	n = tau_spec.components
 	m = n
 	tau_amp = tau_spec.params[:n]
@@ -77,14 +85,14 @@ for source in LMC_list:
 
 	tau_check = np.round(tau_spec.params[2*n:])
 
-#	begin with 3 SNR and 3 min width then go to (5,1)
+	# Changing fitting criteria, and reperforming Gaussian decomposition
 	abs_sig = 5.0
 	abs_min = 1.0
 	tau_spec = Plotting_Spectra(filename, abs_obj.vrad, abs_obj.tau_raw, abs_min, abs_sig)
 	tau_spec.gpp_parameters()
 	n = tau_spec.components
 
-#       this code makes sure that the same components aren't being fit twice
+	# this code makes sure that the same components aren't being fitted twice
 	for i in range(0,n):
 		if round(tau_spec.params[2*n + i]) in tau_check:
 			continue
@@ -93,7 +101,9 @@ for source in LMC_list:
 		tau_mean = np.append(tau_mean, tau_spec.params[2*n + i])
 		m += 1
 
-	for i in range(0,1):	# m instead of 1, simply done for figure.
+	# fit a ridge line to brightness temperature vs optical depth about the full width at half maximum
+	# and store the parameters in a list
+	for i in range(0,1):	# replace with m (number of absorption components) instead of 1, simply done for figure.
 		amp = tau_amp[i]
 		fwhm = tau_fwhm[i]
 		mean = tau_mean[i]
@@ -109,6 +119,7 @@ for source in LMC_list:
 
 cloud_spin_list = list()
 
+# rounding properties to 2 decimal places
 for b_list in a_list:
 	filename, amp, fwhm, mean, T_ew, m, T_c, error = b_list
 	amp = round(amp, 2)
@@ -118,56 +129,32 @@ for b_list in a_list:
 	m = round(m, 2)
 	T_c = round(T_c, 2)
 	error = round(error, 2)
-
-#	print(filename + ": " + str(amp) + ", " + str(fwhm) + ", " + str(mean) + ", " + str(T_ew) + ", " + str(m) + ", " + str(T_c) + ", " + str(error))
-
 	cloud_spin_list.append(T_c)
 
-##
-
+# These are arrays from both absorption and emission spectra
 em_T_B = em_obj.T_B
 em_vrad = em_obj.vrad
 abs_EW = (1 - np.exp(-abs_obj.tau_raw))
 abs_vrad = abs_obj.vrad
 
-# Find index where optical depth maximal (151). Then take a velocity min and max about this index.
-# Then find velocity inidices for T_B that are near this min/max. Then take T_B values
-
-#vrad_min = abs_vrad[151-2]
-#vrad_max = abs_vrad[151+2]
-
-#def find_nearest(array, value):
-#	array = abs(array - value)
-#	index = np.argmin(array)
-#	return index
-
-#min_index = find_nearest(em_vrad, vrad_min)
-#max_index = find_nearest(em_vrad, vrad_max)
-
-#ye_EW = abs_EW[151-2: 151+2]
-#ye_T_B = em_T_B[min_index: max_index]
-
 from scipy.interpolate import interp1d
 
+# interpolate such that the absorption and emission spectra have the same number of values
 f_interp = interp1d(em_vrad, em_T_B, kind='quadratic', bounds_error=False)
 new_T_B = f_interp(abs_vrad)
 
+# roughly the points from the raw emission and absorption spectra that are in the full width at half maximum of detection.
 ye_EW = abs_EW[151-2:151+2]
 ye_T_B = new_T_B[151-2:151+2]
 
-##
-
+# we then plot our ridge line fitting
 print(cloud_spin_list)
-#x = np.linspace(int(mean - 5), int(mean + 5), 1000)
 plt.plot(EW_func(amp, fwhm, mean, x), T_B_model(x), label=r"Model Comparison")
 x = np.linspace(-0.05, 0.6, 100)
 plt.plot(x, x*m + T_ew, label=r"Ridge Line ($q=0.5$)", linestyle='--')
 plt.scatter(ye_EW, ye_T_B, color="green", marker="x", label="Raw Spectra")
-#plt.plot(EW_func(amp, fwhm, mean, x), EW_func(amp, fwhm, mean, x)*m + T_ew, label="Ridge Line ($q=0.5$)", linestyle='--')
 plt.xlabel(r"$(1 - e^{-\tau(v)})$")
 plt.ylabel(r"$T_{B}(v)$ (K)")
 plt.grid()
 plt.legend()
 plt.show()
-#plt.savefig("exampleRidgeLine.pdf")
-#print(np.polyfit(EW_func(x), T_B_model(x), 1))
